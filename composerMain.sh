59d30de2f772ec5e2d4714f7e59e8023b3942266
JulianQuinn52 <45634035+JulianQuinn52@users.noreply.github.com>
2022-02-22 17:24:50 -0500

    Delete composerMain.sh

--- composerMain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CURRENT_USER=$(whoami)
- echo julian1 | sshpass -p "julian1" ssh -tt -o StrictHostKeyChecking=No jq52@10.242.196.151 "sudo service rabbitmq-server start "
- #echo h8 | sshpass -p "h8" ssh -tt -o StrictHostKeyChecking=No jq53@10.242.109.103 "sudo service rabbitmq-server start "
-echo password | sshpass "password" ssh -tt -o StrictHostKeyChecking=No casey@10.242.222.211 "sudo service mysql start"
-echo Cameron10# | sshpass "Cameron10#" ssh -tt -o StrictHostKeyChecking=No cameron9167@10.242.244.173 "echo hostname -I"
-echo dpp58@njit.edu | sshpass "dpp58@njit.edu" ssh -tt -o StrictHostKeyChecking=No Deep@10.242.164.102 "echo hostname -I"
